afef6fe16d683dd2dd5c60db402d85c1b4bcc51a
derekcool38 <76213425+derekcool38@users.noreply.github.com>
2021-03-23 19:30:27 -0400

    Update Loader.lua

--- ScriptHub/Loader.lua
+++ ScriptHub/Loader.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 2512643572 then -- Bubble Gum Simulator
     Utilities:Load("https://raw.githubusercontent.com/derekcool38/Releases/main/Scripts/BubbleGumSimulator.lua")
 elseif game.PlaceId == 6186867282 then -- Project XL
     Utilities:Load("https://raw.githubusercontent.com/derekcool38/Releases/main/Scripts/ProjectXL.lua")
-elseif game.PlaceId == 3311165597 then
+elseif game.PlaceId == 3311165597 then -- Dragon blox ultimate 
     Utilities:Load("https://raw.githubusercontent.com/derekcool38/Releases/main/Scripts/Dragon-Blox-Ultimate.lua")
 else
     game:GetService("Players").LocalPlayer:Kick("[YerPeq Hub] Game not Supported")
